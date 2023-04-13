82885965fc4780231cefd0e1b781d9c37744d7be
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-04-13 05:56:44 -0300

    Inserindo 4_HelloWorld_Classificacao.py

---
+++ 4_HelloWorld_Classificacao.py
@@ -0,0 +1,12 @@
+# Hello World - Classificação 4 - CLASSIFICACAÇÃO DE USUÁRIOS WEB
+
+import pandas as pd
+
+df_acessos = pd.read_csv('https://raw.githubusercontent.com/alura-cursos/machine-learning-introducao-a-classificacao/master/acesso.csv')
+print(df_acessos)
+
+
+
+
+
+
