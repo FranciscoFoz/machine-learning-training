c662c2f15342f38e18317a20fdaa57d9c9c4b4c4
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-04-20 06:18:17 -0300

    Inserindo leitura dos dados csv

--- 4_HelloWorld_Classificacao.py
+++ 4_HelloWorld_Classificacao.py
@@ -1,9 +1,28 @@
 # Hello World - Classificação 4 - CLASSIFICACAÇÃO DE USUÁRIOS WEB
 
-import pandas as pd
+import requests
+import csv 
 
-df_acessos = pd.read_csv('https://raw.githubusercontent.com/alura-cursos/machine-learning-introducao-a-classificacao/master/acesso.csv')
-print(df_acessos)
+url_csv = 'https://raw.githubusercontent.com/alura-cursos/machine-learning-introducao-a-classificacao/master/acesso.csv'
+
+def carregar_acessos(url):
+    
+    dados_raw = requests.get(url).content.decode('utf-8')
+    
+    X = []
+    Y = []
+    
+    leitor = csv.reader(dados_raw.splitlines())
+    next(leitor)
+    for home, como_funciona, contato,comprou in leitor:
+        
+        dado = [int(home), int(como_funciona),int(contato)]
+        X.append(dado)
+        Y.append([int(comprou)])
+        
+    return X, Y
+        
+X, Y = carregar_acessos(url_csv)
 
 
 
