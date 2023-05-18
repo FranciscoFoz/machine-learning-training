6b04e62e23346d92c2369cc13a98079a3ce4529f
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-05-18 07:34:37 -0300

    Continuando validação do modelo

--- Aprendizado Supervisionado/Classificação/6_Validacao_Modelo.py
+++ Aprendizado Supervisionado/Classificação/6_Validacao_Modelo.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
 from sklearn.svm import LinearSVC
 from sklearn.dummy import DummyClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -17,9 +18,9 @@ dados = pd.read_csv(uri).drop(columns=["Unnamed: 0"], axis=1)
 x = dados[["preco", "idade_do_modelo", "km_por_ano"]]
 y = dados["vendido"]
 
+SEED = 135135
 
 # Dividindo em treino e teste
-SEED = 564164
 np.random.seed(SEED)
 treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.25,stratify = y)
 
@@ -29,7 +30,24 @@ print(20*'-')
 # DUMMY CLASSIFIER - Baseline
 dummy_stratified = DummyClassifier(strategy='stratified')
 dummy_stratified.fit(treino_x, treino_y)
-acuracia = dummy_stratified.score(teste_x, teste_y) * 100
+acuracia_dummy = dummy_stratified.score(teste_x, teste_y) * 100
 
-print("\nA acurácia do dummy stratified foi de %.2f%%" % acuracia)
+print("\nA acurácia do dummy stratified foi de %.2f%%" % acuracia_dummy)
 print(20*'-')
+
+# ÁRVORE DE DECISÃO
+dtc = DecisionTreeClassifier(max_depth = 2)
+dtc.fit(treino_x, treino_y)
+acuracia_dtc = dtc.score(teste_x, teste_y) * 100
+
+print("\nA acurácia do decision tree classifier foi de %.2f%%" % acuracia_dtc)
+print(20*'-')
+
+# Validação cruzada
+dtc = DecisionTreeClassifier(max_depth = 2)
+results = cross_validate(dtc,x,y,cv=3,return_train_score=False)
+media_resultados = results['test_score'].mean()
+desvio_padrao_resultados = results['test_score'].std()
+
+
+print(results['test_score'].mean)
