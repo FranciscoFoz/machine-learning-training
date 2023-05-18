6a12b4b3508665435f53ec83d90324f9c1fe4eb0
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-05-18 18:32:46 -0300

    Continuando curso validação cruzada

--- Aprendizado Supervisionado/Classificação/6_Validacao_Modelo.py
+++ Aprendizado Supervisionado/Classificação/6_Validacao_Modelo.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate
+from sklearn.model_selection import KFold
 from sklearn.svm import LinearSVC
 from sklearn.dummy import DummyClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -18,7 +19,7 @@ dados = pd.read_csv(uri).drop(columns=["Unnamed: 0"], axis=1)
 x = dados[["preco", "idade_do_modelo", "km_por_ano"]]
 y = dados["vendido"]
 
-SEED = 135135
+SEED = 1
 
 # Dividindo em treino e teste
 np.random.seed(SEED)
@@ -44,10 +45,32 @@ print("\nA acurácia do decision tree classifier foi de %.2f%%" % acuracia_dtc)
 print(20*'-')
 
 # Validação cruzada
+'''
 dtc = DecisionTreeClassifier(max_depth = 2)
-results = cross_validate(dtc,x,y,cv=3,return_train_score=False)
+results = cross_validate(dtc,x,y,cv=5,return_train_score=False)
 media_resultados = results['test_score'].mean()
 desvio_padrao_resultados = results['test_score'].std()
 
+acuracia_results_min = round((media_resultados - (2 * desvio_padrao_resultados)) * 100,2)
+acuracia_results_max = round((media_resultados + (2 * desvio_padrao_resultados)) * 100,2)
 
-print(results['test_score'].mean)
+
+print(f'Intervalo da acuracia do Decision Tree Classifier\ncom validação cruzada: [{acuracia_results_min},{acuracia_results_max}]')
+print(20*'-')
+'''
+# Aleatoriedade com validação cruzada
+
+def imprime_resultados(results):
+    media_resultados = results['test_score'].mean().round(2)
+    desvio_padrao_resultados = results['test_score'].std()
+    acuracia_results_min = round((media_resultados - (2 * desvio_padrao_resultados)) * 100,2)
+    acuracia_results_max = round((media_resultados + (2 * desvio_padrao_resultados)) * 100,2)
+    
+    print(f'Média: {media_resultados * 100}')
+    print(f'Intervalo da acuracia do Decision Tree Classifier\ncom validação cruzada: [{acuracia_results_min},{acuracia_results_max}]')
+
+
+cv = KFold(n_splits = 5,shuffle= True)
+dtc = DecisionTreeClassifier(max_depth = 2)
+results = cross_validate(dtc,x,y,cv=cv,return_train_score=False)
+imprime_resultados(results)
