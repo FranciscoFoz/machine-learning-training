d4214ba5316e1bea7b417567cba740c655f8124c
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-05-25 09:43:29 -0300

    Finalizando 6 Validação Cruzada

--- Aprendizado Supervisionado/Classificação/6_Validacao_Modelo.py
+++ Aprendizado Supervisionado/Classificação/6_Validacao_Modelo.py
@@ -7,26 +7,31 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import KFold
 from sklearn.model_selection import StratifiedKFold
-from sklearn.svm import LinearSVC
+from sklearn.model_selection import GroupKFold
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.svm import SVC
 from sklearn.dummy import DummyClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
-#Importando dados
-uri = "https://gist.githubusercontent.com/guilhermesilveira/e99a526b2e7ccc6c3b70f53db43a87d2/raw/1605fc74aa778066bf2e6695e24d53cf65f2f447/machine-learning-carros-simulacao.csv"
-dados = pd.read_csv(uri).drop(columns=["Unnamed: 0"], axis=1)
-dados = 
-
-print(dados)
-
-#Definindo x e y
-x = dados[["preco", "idade_do_modelo", "km_por_ano"]]
-y = dados["vendido"]
+from sklearn.pipeline import Pipeline
 
 SEED = 1
 
+#Importando dados
+uri = "https://gist.githubusercontent.com/guilhermesilveira/e99a526b2e7ccc6c3b70f53db43a87d2/raw/1605fc74aa778066bf2e6695e24d53cf65f2f447/machine-learning-carros-simulacao.csv"
+dados = pd.read_csv(uri).drop(columns=["Unnamed: 0"], axis=1)
+dados['modelo'] = dados.idade_do_modelo + np.random.randint(-2,3, size=10000)
+dados['modelo'] = dados['modelo'] + abs(dados['modelo'].min()) + 1
+print(dados)
+
+#Definindo x e y
+x = dados[["preco", "idade_do_modelo", "km_por_ano","modelo"]]
+y = dados["vendido"]
+
 # Dividindo em treino e teste
-np.random.seed(SEED)
 treino_x, teste_x, treino_y, teste_y = train_test_split(x, y, test_size = 0.25,stratify = y)
 
 print(5*'+-','INÍCIO DO PROGRAMA',5*'-+')
@@ -53,6 +58,20 @@ print(5*'+-','DECISION TREE CLASSIFIER CLASSIFIER',5*'-+')
 print("A acurácia foi de %.2f%%" % acuracia_dtc)
 print(quantidade_de_tracos*'-')
 
+#SVC
+scaler = StandardScaler()
+scaler.fit(treino_x)
+treino_x_escalado = scaler.transform(treino_x)
+teste_x_escalado = scaler.transform(teste_x)
+
+svc = SVC()
+svc.fit(treino_x_escalado, treino_y)
+acuracia_svc = svc.score(teste_x_escalado, teste_y) * 100
+
+print(5*'+-','SVC COM SCALER',5*'-+')
+print("A acurácia foi de %.2f%%" % acuracia_svc)
+print(quantidade_de_tracos*'-')
+
 # Validação cruzada
 '''
 dtc = DecisionTreeClassifier(max_depth = 2)
@@ -69,7 +88,7 @@ print(20*'-')
 '''
 
 
-# Aleatoriedade com validação cruzada
+# Validação cruzada
 
 def imprime_resultados(results):
     media_resultados = results['test_score'].mean().round(2)
@@ -91,13 +110,50 @@ imprime_resultados(results)
 
 print(quantidade_de_tracos*'-')
 
-# Aleatoriedade com validação cruzada estratificada
+# Validação cruzada estratificada
 
 print(5*'+-','DECISION TREE CLASSIFIER COM VALIDAÇÃO ESTRATIFICADA',5*'-+')
 
 scv = StratifiedKFold(n_splits = 5,shuffle= True)
 dtc = DecisionTreeClassifier(max_depth = 2)
 results = cross_validate(dtc,x,y,cv=scv,return_train_score=False)
+imprime_resultados(results)
+
+print(quantidade_de_tracos*'-')
+
+# Validação cruzada por grupos
+
+print(5*'+-','DECISION TREE CLASSIFIER COM VALIDAÇÃO CRUZADA POR GRUPOS',5*'-+')
+
+gkf = GroupKFold(n_splits = 5)
+dtc = DecisionTreeClassifier(max_depth = 2)
+results = cross_validate(dtc,x,y,cv=gkf,groups=dados.modelo,return_train_score=False)
+imprime_resultados(results)
+
+print(quantidade_de_tracos*'-')
+
+# SVC e Scaler com validação cruzada por grupos
+
+print(5*'+-','SVC E SCALER COM VALIDAÇÃO CRUZADA POR GRUPOS',5*'-+')
+
+gkf = GroupKFold(n_splits = 5)
+svc = SVC()
+results = cross_validate(svc,x,y,cv=gkf,groups=dados.modelo,return_train_score=False)
+imprime_resultados(results)
+
+print(quantidade_de_tracos*'-')
+
+
+#Pipeline
+print(5*'+-','PIPELINE COM SVC',5*'-+')
+
+scaler = StandardScaler()
+svc = SVC()
+
+pipeline = Pipeline([('transformacao',scaler),('estimador',svc)])
+
+gkf = GroupKFold(n_splits = 5)
+results = cross_validate(pipeline,x,y,cv=gkf,groups=dados.modelo,return_train_score=False)
 imprime_resultados(results)
 
 print(quantidade_de_tracos*'-')
