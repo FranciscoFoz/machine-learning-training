cd4df6ffc36c050d68cab945ae313381db84183d
FranciscoFoz <64700794+FranciscoFoz@users.noreply.github.com>
2023-08-28 06:45:13 -0300

    Iniciando curso mklops-deploy

--- MLOps/1_Curso_MLOps/MLOps_ambiente_virtual/mlops-deploy/src/app/main.py
+++ MLOps/1_Curso_MLOps/MLOps_ambiente_virtual/mlops-deploy/src/app/main.py
@@ -3,14 +3,16 @@ from flask_basicauth import BasicAuth
 from textblob import TextBlob
 import pickle
 from sklearn.linear_model import LinearRegression
+import os
 
 colunas = ['tamanho','ano','garagem']
-modelo = pickle.load(open('/home/franciscofoz/Documents/GitHub/machine-learning-training/MLOps/1_Curso_MLOps/modelo.sav','rb'))
+#modelo = pickle.load(open('../../models/modelo.sav','rb'))
+modelo = pickle.load(open('MLOps/1_Curso_MLOps/MLOps_ambiente_virtual/mlops-deploy/models/modelo.sav','rb'))
 
 
 app = Flask(__name__)
-app.config['BASIC_AUTH_USERNAME'] = 'julio'
-app.config['BASIC_AUTH_PASSWORD'] = 'alura'
+app.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+app.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
 
 basic_auth = BasicAuth(app)
 
@@ -37,6 +39,6 @@ def cotacao():
     return jsonify(preco=preco[0])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
     
 
